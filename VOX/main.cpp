467e19a501a1216ac4b516c2d87c5460bb0deeba
Nishiyama Nobuyuki <tek.nishi@gmail.com>
2019-10-08 14:20:46 +0900

    Initial Commit

---
+++ VOX/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  VOX
+//
+//  Created by Nishiyama Nobuyuki on 2019/10/08.
+//  Copyright © 2019 Nishiyama Nobuyuki. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
